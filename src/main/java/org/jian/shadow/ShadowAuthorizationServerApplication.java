a14faec936db45363185a5ba1d9bfff81b76a578
jian <janine.jian.chen@gmail.com>
2019-12-02 14:03:33 +0800

    init springboot project

---
+++ src/main/java/org/jian/shadow/ShadowAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package org.jian.shadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowAuthorizationServerApplication.class, args);
+	}
+
+}

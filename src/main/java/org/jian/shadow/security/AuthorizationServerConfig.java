2c0582ec0115ea65af405d1683019b9a9bd776fb
jian <janine.jian.chen@gmail.com>
2019-12-03 09:37:08 +0800

    1. custom passwordEncoder 2. custom clientDetailsService

--- src/main/java/org/jian/shadow/security/AuthorizationServerConfig.java
+++ src/main/java/org/jian/shadow/security/AuthorizationServerConfig.java
@@ -4,16 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -21,21 +32,41 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public DataSource dataSource;
     @Autowired
     public AuthenticationManager authenticationManager;
+    @Autowired
+    public PasswordEncoder passwordEncoder;
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         endpoints.authenticationManager(authenticationManager);
-        //.tokenStore(tokenStore())
-        //.accessTokenConverter(jwtAccessTokenConverter());
     }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.jdbc(dataSource);
+        clients.withClientDetails(clientDetailsService());
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) {
         security.checkTokenAccess("isAuthenticated()");
+    }
+
+    public ClientDetailsService clientDetailsService() {
+        return new ClientDetailsService() {
+            @Override
+            public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
+                BaseClientDetails details = new BaseClientDetails();
+                details.setClientId(clientId);
+                details.setAuthorizedGrantTypes(Arrays.asList("authorization_code","password","refresh_token") );
+                details.setScope(Arrays.asList("all"));
+                details.setRegisteredRedirectUri(Collections.singleton("http://localhost:8080"));
+                details.setResourceIds(Arrays.asList("resource"));
+                Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+                authorities.add(new SimpleGrantedAuthority("ROLE_CLIENT"));
+                details.setAuthorities(authorities);
+                details.setClientSecret(passwordEncoder.encode("oauth2") );
+                return details;
+            }
+        };
     }
 
     @Bean

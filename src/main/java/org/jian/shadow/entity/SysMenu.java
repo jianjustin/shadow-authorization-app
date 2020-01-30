4c13bdb9941d2d3960cf650313ef6288f54d3770
jian <janine.jian.chen@gmail.com>
2020-01-30 11:58:18 +0800

    remove dependency spring-data-jpa

--- src/main/java/org/jian/shadow/entity/SysMenu.java
+++ src/main/java/org/jian/shadow/entity/SysMenu.java
@@ -1,38 +1,24 @@
 package org.jian.shadow.entity;
 
 
-import javax.persistence.*;
-
 /**
  * 系统菜单表 - sys_menu
  */
-@Table(name = "sys_menu")
-@Entity
 public class SysMenu {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "id")
     public int id;
 
-    @Column(name = "parentId")
     public int parentId;
 
-    @Column(name = "menuName")
     public String menuName;
 
-    @Column(name = "menuIcon")
     public String menuIcon;
 
-    @Column(name = "menuUrl")
     public String menuUrl;
 
-    @Column(name = "menuType")
     public String menuType;
 
-    @Column(name = "menuOrder")
     public String menuOrder;
 
-    @Column(name = "menuStatus")
     public String menuStatus;
 }

c952d3352f6de85dc3b8624dee6fcba877d7ce64
jian <janine.jian.chen@gmail.com>
2020-01-31 11:30:19 +0800

    add homecontroller

---
+++ src/main/java/org/jian/shadow/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.jian.shadow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "this is a oauth app based spring boot";
+    }
+}

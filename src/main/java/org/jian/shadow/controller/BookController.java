ec9712477ae635ea490c505e54e9b9ce834828eb
chenjianrui <janine.jian.chen@gmail.com>
2021-02-15 23:17:18 +0800

    修改HTTP编码方式

--- src/main/java/org/jian/shadow/controller/BookController.java
+++ src/main/java/org/jian/shadow/controller/BookController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,15 +18,16 @@ public class BookController {
     @Autowired
     public BookService bookService;
 
-    @GetMapping("/book")
+    @GetMapping(value="/book", produces = "application/json;charset=utf-8", consumes = "application/json;charset=utf-8")
     @PreAuthorize("hasAuthority('book.query')")
     @ShadowLog(description = "查询所有书籍")
     public List<Book> findAll(){
         return bookService.findAll();
     }
 
-    @PostMapping("/book/findBooksByClassify")
+    @PostMapping(value = "/book/findBooksByClassify", produces = "application/json;charset=utf-8", consumes = "application/json;charset=utf-8")
     @PreAuthorize("hasAuthority('book.query')")
+    @ResponseBody()
     @ShadowLog(description = "根据分类查询所有书籍")
     public Page<Book> findBooksByClassify(@RequestBody BookQueryView bookQueryView){
         PageRequest pageRequest = PageRequest.of(0,10);
@@ -38,7 +36,7 @@ public class BookController {
         return bookService.findBooksByClassify(bookQueryView.getClassify(), pageRequest);
     }
 
-    @PostMapping("/book/findBooksByNameIsLike")
+    @PostMapping(value = "/book/findBooksByNameIsLike", produces = "application/json;charset=utf-8", consumes = "application/json;charset=utf-8")
     @PreAuthorize("hasAuthority('book.query')")
     @ShadowLog(description = "根据标题查询所有书籍")
     public Page<Book> findBooksByNameIsLike(@RequestBody BookQueryView bookQueryView){
--- src/main/java/org/jian/shadow/controller/ClassifyController.java
+++ src/main/java/org/jian/shadow/controller/ClassifyController.java
@@ -2,9 +2,7 @@ package org.jian.shadow.controller;
 
 import org.jian.shadow.common.log.ShadowLog;
 import org.jian.shadow.domain.Classify;
-import org.jian.shadow.domain.SysPermission;
 import org.jian.shadow.service.ClassifyService;
-import org.jian.shadow.service.SysPermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +15,7 @@ public class ClassifyController {
     @Autowired
     public ClassifyService classifyService;
 
-    @GetMapping("/classify")
+    @GetMapping(value="/classify", produces = "application/json;charset=utf-8", consumes = "application/json;charset=utf-8")
     @PreAuthorize("hasAuthority('classify.query')")
     @ShadowLog(description = "查询所有分类")
     public List<Classify> findAll(){

8b337a542e5eb0b44be757e3dc19e1e76603a5c5
极北之地 <janine.jian.chen@gmail.com>
2021-05-05 16:03:48 +0800

    生成user单表api

--- src/main/kotlin/org/janine/jian/repository/ArticleRepository.kt
+++
@@ -1,9 +0,0 @@
-package org.janine.jian.repository
-
-import org.janine.jian.entity.Article
-import org.springframework.data.repository.CrudRepository
-
-interface ArticleRepository : CrudRepository<Article, Long> {
-    fun findBySlug(slug: String): Article?
-    fun findAllByOrderByAddedAtDesc(): Iterable<Article>
-}
--- src/main/kotlin/org/janine/jian/rest/UserController.kt
+++ src/main/kotlin/org/janine/jian/rest/UserController.kt
@@ -1,21 +1,28 @@
 package org.janine.jian.rest
 
+import org.janine.jian.entity.User
 import org.janine.jian.repository.UserRepository
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
+import java.util.*
 
 @RestController
 @RequestMapping("/api/user")
 class UserController(private val repository: UserRepository) {
 
     @GetMapping("/")
-    fun findAll() = repository.findAll()
+    fun findAll(): List<User>{
+        return repository.findAll()?.toList();
+    }
 
     @GetMapping("/{login}")
-    fun findOne(@PathVariable login: String) =
-            repository.findByLogin(login) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
+    fun findOne(@PathVariable login: String): User{
+        return repository.findByLogin(login) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist");
+    }
+
+    @PostMapping("/", )
+    fun save(@RequestBody user: User): User{
+        return repository.save(user);
+    }
 }

aa9ea2040cfd56229e1d6f319dc4dbf477915908
极北之地 <janine.jian.chen@gmail.com>
2021-05-05 10:40:50 +0800

    添加springboot web依赖

--- src/main/kotlin/org/janine/jian/ShadowAuthorizationAppApplication.kt
+++ src/main/kotlin/org/janine/jian/ShadowAuthorizationAppApplication.kt
@@ -1,5 +1,6 @@
 package org.janine.jian
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class ShadowAuthorizationAppApplication
 
 fun main(args: Array<String>) {
-	runApplication<ShadowAuthorizationAppApplication>(*args)
+	runApplication<ShadowAuthorizationAppApplication>(*args){
+		setBannerMode(Banner.Mode.OFF)
+	}
 }

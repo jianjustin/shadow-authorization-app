106c0b8c62d4910ead3518cdd7a54f90202461ed
极北之地 <janine.jian.chen@gmail.com>
2021-05-05 15:25:13 +0800

    增加Mustache依赖

---
+++ src/main/kotlin/org/janine/jian/rest/HtmlController.kt
@@ -0,0 +1,15 @@
+package org.janine.jian.rest
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+    @GetMapping("/")
+    fun blog(model: Model): String {
+        model["title"] = "Blog"
+        return "blog"
+    }
+}
